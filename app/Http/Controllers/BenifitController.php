e6556bc0c562cecde03e2a20280eb8c1311cf086
ManhTu <gautrangcb91@gmail.com>
2020-03-23 09:18:49 +0700

    validate

--- app/Http/Controllers/BenifitController.php
+++ app/Http/Controllers/BenifitController.php
@@ -4,7 +4,65 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Benifit;
+
+use App\Http\Requests\AddNewBenifitRequest;
+use App\Http\Requests\EditBenifitRequest;
+
 class BenifitController extends Controller
 {
-    //
+	public function __construct()
+	{
+		$this->benifit = new Benifit;
+	}
+
+	public function listBenifit(Request $request)
+	{
+		$paginate = config('constants.PAGINATE_BENIFIT');
+		$listBenifit = $this->benifit->listBenifitPaginate($request, $paginate);
+		return view('pages.list-benifit', compact('listBenifit'));
+	}
+
+	public function infoBenifit($idBenifit)
+	{
+		$infoRider = $this->benifit->infoBenifitById($idBenifit);
+		return view();
+	}
+
+	public function addNewBenifit(AddNewBenifitRequest $request)
+	{
+		return $request;
+		$param = [
+			'name' => $request->name,
+			'detail' => $request->detail,
+			'rider_id' => $request->rider_id
+		];
+		$resultAddBenifit = $this->benifit->addNewBenifit($param);
+		if ($resultAddBenifit) {
+			return view();
+		} else {
+			return view();
+		}
+	}
+
+	public function editBenifit(EditBenifitRequest $request, $idBenifit)
+	{
+		return $request;
+		$infoBenifit = $this->benifit->infoBenifitById($idBenifit);
+		if ($infoBenifit) {
+			$param = [
+				'name' => $request->name,
+				'detail' => $request->detail,
+				'rider_id' => $request->rider_id
+			];
+			$resultEditBenifit = $this->benifit->editBenifit($idBenifit, $param);
+			if ($resultEditBenifit) {
+				return view();
+			} else {
+				return view();
+			}
+		} else {
+			return view();
+		}
+	}
 }
--- app/Http/Controllers/RiderController.php
+++ app/Http/Controllers/RiderController.php
@@ -20,9 +20,44 @@ class RiderController extends Controller
 		return view('pages.list-rider', compact('listRider'));
 	}
 
-	public function infoRider($idPlan)
+	public function infoRider($idRider)
 	{
-		$infoRider = $this->rider->infoRiderById($idCategoryPlan);
+		$infoRider = $this->rider->infoRiderById($idRider);
 		return view();
+	}
+
+	public function addNewRider(Request $request)
+	{
+		return $request;
+		$param = [
+			'name' => $request->name,
+			'feature' => $request->feature
+		];
+		$resultAddRider = $this->rider->addNewRider($param);
+		if ($resultAddRider) {
+			return view();
+		} else {
+			return view();
+		}
+	}
+
+	public function editRider(Request $request, $idRider)
+	{
+		return $request;
+		$infoRider = $this->rider->infoRiderById($idRider);
+		if ($infoRider) {
+			$param = [
+				'name' => $request->name,
+				'feature' => $request->feature
+			];
+			$resultEditRider = $this->rider->editRider($idRider, $param);
+			if ($resultEditRider) {
+				return view();
+			} else {
+				return view();
+			}
+		} else {
+			return view();
+		}
 	}
 }
