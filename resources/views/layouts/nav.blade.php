f7226985b584df7dcdae9b7e0674537c77d12d6b
Tu Nguyen Viet <gamethuthanhnam@gmail.com>
2020-03-25 15:22:41 +0700

    update style

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,21 +1,21 @@
 <nav>
     <ul class="mainmenu">
-        <li class="{{ Route::currentRouteNamed('home') ? 'active' : '' }}"><a href="{{route('home')}}"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
-        <li class="has-child"><a href=""><i class="fas fa-plus-circle"></i><span>ADD PFR</span></a>
+        <li class="{{ Route::currentRouteNamed('home') ? 'active' : '' }}"><a href="{{route('home')}}" title="Home"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
+        <li class="has-child "><a href="Javascript:;" title="ADD PFR"><i class="fas fa-plus-circle"></i><span>ADD PFR</span></a>
             <ul class="sub-menu">
-                <li class="{{ Route::currentRouteNamed('single-fact.add_new') ? 'active' : '' }}"><a href="{{route('single-fact.add_new')}}"><i class="fas fa-user"></i>Single Fact - Find</a></li>
-                <li class="{{ Route::currentRouteNamed('single-fact.add_new') ? 'active' : '' }}"><a href="{{route('joint-fact.add_new')}}"><i class="fas fa-users"></i>Joint Fact - Find</a></li>
+                <li class="{{ Route::currentRouteNamed('single-fact.add_new') ? 'active' : '' }}"><a href="{{route('single-fact.add_new')}}" title="Single Fact - Find"><i class="fas fa-user"></i>Single Fact - Find</a></li>
+                <li class="{{ Route::currentRouteNamed('joint-fact.add_new') ? 'active' : '' }}"><a href="{{route('joint-fact.add_new')}}" title="Joint Fact - Find"><i class="fas fa-users"></i>Joint Fact - Find</a></li>
             </ul>
         </li>
-        <li class="{{ Route::currentRouteNamed('listpfr') ? 'active' : '' }}"><a href="{{route('listpfr')}}"><i class="fas fa-list-alt"></i><span>View List PFR</span></a></li>
-        <li class="{{ Route::currentRouteNamed('user.list') ? 'active' : '' }}"><a href="{{route('user.list')}}"><i class="fas fa-user-circle"></i><span>User</span></a></li>
-        <li class="{{ Route::currentRouteNamed('plan.list') ? 'active' : '' }}"><a href="{{route('plan.list')}}"><i class="fas fa-clipboard-list"></i><span>Plans</span></a>
+        <li class="{{ Route::currentRouteNamed('listpfr') ? 'active' : '' }}"><a href="" title="View List PFR"><i class="fas fa-list-alt"></i><span>View List PFR</span></a></li>
+        <li class="{{ Route::currentRouteNamed('user.list') ? 'active' : '' }}"><a href="{{route('user.list')}}" title="User"><i class="fas fa-user-circle"></i><span>User</span></a></li>
+        <li class="{{ Route::currentRouteNamed('plan.list') ? 'active' : '' }} has-child"><a href="{{route('plan.list')}}" title="Plans"><i class="fas fa-clipboard-list"></i><span>Plans</span></a>
             <ul class="sub-menu">
-                <li class="{{ Route::currentRouteNamed('plan.list') ? 'active' : '' }}"><a href="{{route('plan.list')}}"><i class="fas fa-user"></i>List Plan</a></li>
-                <li class="{{ Route::currentRouteNamed('category.list') ? 'active' : '' }}"><a href="{{route('category.list')}}"><i class="fas fa-users"></i>List Category</a></li>
+                <li class="{{ Route::currentRouteNamed('plan.list') ? 'active' : '' }}"><a href="{{route('plan.list')}}" title="List Plan"><i class="fas fa-caret-right"></i>List Plan</a></li>
+                <li class="{{ Route::currentRouteNamed('category.list') ? 'active' : '' }}"><a href="{{route('category.list')}}" title="List Category"><i class="fas fa-caret-right"></i>List Category</a></li>
             </ul>
         </li>
-        <li class="{{ Route::currentRouteNamed('riders.list') ? 'active' : '' }}"><a href="{{route('riders.list')}}"><i class="fas fa-clipboard-list"></i><span>Ridders</span></a></li> 
-        <li class="{{ Route::currentRouteNamed('company.list') ? 'active' : '' }}"><a href="{{route('company.list')}}"><i class="fas fa-clipboard-list"></i><span>Company</span></a></li>  
+        <li class="{{ Route::currentRouteNamed('riders.list') ? 'active' : '' }}"><a href="{{route('riders.list')}}" title="Riders"><i class="fas fa-file-alt"></i><span>Riders</span></a></li> 
+        <li class="{{ Route::currentRouteNamed('company.list') ? 'active' : '' }}"><a href="{{route('company.list')}}" title="Company"><i class="fas fa-building"></i><span>Company</span></a></li>  
     </ul>
 </nav>
