b779aa201dcc2885e8ee56c032c9d4b2f91b8544
Tu Nguyen Viet <gamethuthanhnam@gmail.com>
2020-03-30 15:07:40 +0700

    updated home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,11 +7,12 @@
         <h4>Please select your desired application:</h4>
     </div>
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 borderfullwidth link-action-addnew">
-        <a class="textcenter radius_4" href="">Single Fact Find</a>
-        <a class="textcenter radius_4" href="">Joint Fact Find</a>
+        <a class="textcenter radius_4" href=""><i class="fas fa-user"></i>Single Fact Find</a>
+        <a class="textcenter radius_4" href=""><i class="fas fa-users"></i>Joint Fact Find</a>
     </div>
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <table id="example" class="table table-striped table-bordered table-content" style="width:100%">
+        <a class="link-innerpage" href="{{route('pfr.list')}}">List PRF</a>
+        <table id="table-pfr-home" class="table  table-content table-style1" style="width:100%">
             <thead>
                 <tr>
                     <th>SN</th>
@@ -24,67 +25,31 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>VTNO1</td>
-                    <td>gtn2vt</td>
-                    <td>March 17,2020</td>
-                    <td>joint</td>
-                    <td>Download PDF</td>
-                    <td>Delete</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>VTNO1</td>
-                    <td>gtn2vt</td>
-                    <td>March 17,2020</td>
-                    <td>joint</td>
-                    <td>Download PDF</td>
-                    <td>Delete</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>VTNO1</td>
-                    <td>gtn2vt</td>
-                    <td>March 17,2020</td>
-                    <td>single</td>
-                    <td>Download PDF</td>
-                    <td>Delete</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>VTNO1</td>
-                    <td>gtn2vt</td>
-                    <td>March 17,2020</td>
-                    <td>single</td>
-                    <td>Download PDF</td>
-                    <td>Delete</td>
-                </tr>
-                <tr>
-                    <td>5</td>
-                    <td>VTNO1</td>
-                    <td>gtn2vt</td>
-                    <td>March 17,2020</td>
-                    <td>single</td>
-                    <td>Download PDF</td>
-                    <td>Delete</td>
-                </tr>
-                <tr>
-                    <td>6</td>
-                    <td>VTNO1</td>
-                    <td>gtn2vt</td>
-                    <td>March 17,2020</td>
-                    <td>single</td>
-                    <td>Download PDF</td>
-                    <td>Delete</td>
-                </tr>
+                @foreach($listPfr as $pfr)
+                    <tr>
+                        <td>{{$pfr->id}}</td>
+                        <td>{{$pfr->nameClient}}</td>
+                        <td>{{$pfr->nameUserAdd}}</td>
+                        <td>{{$pfr->createDate}}</td>
+                        <td>{{$pfr->typePfr}}</td>
+                        <td><a class="pdflink" href="{{route('downloadpdf',$pfr->id)}}">Download as PDF</a></td>
+                        <td>
+                            <a href="javascript:;" class="deletestyle1 delete" data-id="{{$pfr->id}}" data-url="{{route('pfr.move_to_trash', $pfr->id)}}"><i class="fas fa-trash"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
-
+        <div class="bottom-table">
+            <div class="viewall-table">
+                <a href="{{route('pfr.list')}}" title="View All">View All</a>
+            </div>
+            <div class="paginate-style">{{$listPfr->links()}}</div>
+        </div>
     </div>
     <div class="section second-section">
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <table id="user-list" class="table table-striped table-bordered table-content" style="width:100%">
+            <table id="user-list" class="table  table-content table-style1" style="width:100%">
                 <thead>
                     <tr>
                         <th>SN</th>
@@ -94,53 +59,25 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>VTNO2</td>
-                        <td>gtn2vt@gmail.com</td>
-                        <td>active</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>VTNO2</td>
-                        <td>gtn2vt@gmail.com</td>
-                        <td>active</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>VTNO2</td>
-                        <td>gtn2vt@gmail.com</td>
-                        <td>active</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>VTNO2</td>
-                        <td>gtn2vt@gmail.com</td>
-                        <td>active</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>VTNO2</td>
-                        <td>gtn2vt@gmail.com</td>
-                        <td>active</td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>VTNO2</td>
-                        <td>gtn2vt@gmail.com</td>
-                        <td>active</td>
-                    </tr>
-                    <tr>
-                        <td>7</td>
-                        <td>VTNO2</td>
-                        <td>gtn2vt@gmail.com</td>
-                        <td>active</td>
-                    </tr>
+                    @foreach($listUser as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->full_name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td>{{$user->status}}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
+            <div class="bottom-table">
+                <div class="viewall-table">
+                    <a href="{{route('user.list')}}" title="View All">View All</a>
+                </div>
+                <div class="paginate-style">{{$listUser->links()}}</div>
+            </div>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <table id="user-list" class="table table-striped table-bordered table-content" style="width:100%">
+            <table id="user-list" class="table  table-content table-style1" style="width:100%">
                 <thead>
                     <tr>
                         <th>SN</th>
@@ -150,50 +87,22 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Aviva</td>
-                        <td>MyLifeChose</td>
-                        <td>Whole Life</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Aviva</td>
-                        <td>MyLifeChose</td>
-                        <td>Whole Life</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Aviva</td>
-                        <td>MyLifeChose</td>
-                        <td>Whole Life</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Aviva</td>
-                        <td>MyLifeChose</td>
-                        <td>Whole Life</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>Aviva</td>
-                        <td>MyLifeChose</td>
-                        <td>Whole Life</td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>Aviva</td>
-                        <td>MyLifeChose</td>
-                        <td>Whole Life</td>
-                    </tr>
-                    <tr>
-                        <td>7</td>
-                        <td>Aviva</td>
-                        <td>MyLifeChose</td>
-                        <td>Whole Life</td>
-                    </tr>
+                     @foreach($listPlan as $plan)
+                        <tr>
+                            <td>{{$plan->id}}</td>
+                            <td>{{$plan->nameCompany}}</td>
+                            <td>{{$plan->name}}</td>
+                            <td>{{$plan->nameCategoryPlan}}</td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
+            <div class="bottom-table">
+                <div class="viewall-table">
+                    <a href="{{route('plan.list')}}" title="View All">View All</a>
+                </div>
+                <div class="paginate-style">{{$listPlan->links()}}</div>
+            </div>
         </div>
     </div>
 
