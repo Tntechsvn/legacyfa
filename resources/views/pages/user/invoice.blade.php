0e3ce43833b73ec229aa7c7d264e25cf9f5fb2be
Tu Nguyen Viet <gamethuthanhnam@gmail.com>
2020-03-30 08:18:35 +0700

    update

--- resources/views/pages/user/invoice.blade.php
+++ resources/views/pages/user/invoice.blade.php
@@ -59,6 +59,49 @@
                 <p><span class="title-info-client1">LFA Code:</span><span class="content-info-client1"></span></p>
                 <p><span class="title-info-client1">Handphone:</span><span class="content-info-client1"></span></p>
             </div>
+            <div class="content-section2">
+                <div class="title2">Important Notice to Client</div>
+                <div class="aboutus1">
+                    <h3>About Legacy FA Private Limited</h3>
+                    <p>Legacy FA Private Limited ("Legacy FA") is a Licensed Financial Adviser authorised under the Financial Advisers Act ("FAA") to conduct the following regulated activities:</p>
+                    <ul>
+                        <li>Advising others, either directly or through publications or writings, and whether in electronic, print or other form, concerning the following investment products, other than -<br/>
+                        (i) in the manner specified in paragraph 2 of the Second Schedule to the Financial Advisers Act (Cap. 110); or<br/>
+                        (ii) advising on corporate finance within the meaning of the Securities and Futures Act (Cap. 289)
+                            <ul>
+                                <li>Life policies</li>
+                                <li>Collective investment schemes</li>
+                            </ul>    
+                        </li>
+                        <li>Advising others by issuing or promulgating research analyses or research reports, whether in electronic, print or other form, concerning the following investment products
+                            <ul>
+                                <li>Life policies</li>
+                                <li>Collective investment schemes</li>
+                            </ul>
+                        </li>
+                        <li>Marketing of any collective investment scheme</li>
+                        <li>Arranging of any contract of insurance in respect of life policies</li>  
+                    </ul>
+                    <p>For full range of our services and list of our product partners, please visit our website at www.legacyfa-asia.com.</p>
+                </div>
+                <div class="aboutus1">
+                    <p>Your Legacy FA Representative is issued with a Representative Number by the Monetary Authority of Singapore (MAS) to conduct regulated activities under the Financial Advisers Act and is authorized to arrange contracts and provide advice on the following products:</p>
+                    <ul>
+                        <li>Life Insurance</li>
+                        <li>Health Insurance</li>
+                        <li>Investment-linked Plans</li>
+                        <li>Collective Investment Schemes (e.g. Unit Trust)</li>
+                    </ul>
+                    <p>Your Legacy FA Representative must have sufficient information before making a suitable recommendation. The information that you provide on your investment objectives, financial situation and your particular needs will be the basis on which advice and recommendation(s) will be given. If there have been any changes in your circumstances since completing this 'Personal Financial Record', please notify your Legacy FA Representative as it may affect the needs analysis process. The recommendations made for you may not be appropriate in the event of a partial or inaccurate completion of this 'Personal Financial Record'.</p>
+                    <p>All the information provided in your 'Personal Financial Record' is strictly confidential and is only to be used for the purpose of gathering information to recommend suitable products and services, and not be used for any other purposes.</p>
+                </div>
+            </div>
+        </div>
+        <div class="content-section3">
+            <div class="section1">
+                <h1>Section 1. Personal Information</h1>
+                <h3>1.1 Client(s) Information</h3>
+            </div>
         </div>
 </body>
 </html>
