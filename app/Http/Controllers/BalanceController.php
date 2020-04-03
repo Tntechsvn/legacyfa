10c8ba9fb4af040f301ce496a041686c6ccd152b
ManhTu <gautrangcb91@gmail.com>
2020-04-03 17:22:57 +0700

    database & calculator js for balance

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -4,10 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Pfr;
+
 class BalanceController extends Controller
 {
-    public function listBalance()
+	public function __construct()
 	{
-		return view('pages.single-fact.balance.list');
+		$this->pfr = new Pfr;
+	}
+
+	public function listBalance($idPfr)
+	{
+		$infoPfr = $this->pfr->infoPfrById($idPfr);
+		return view('pages.single-fact.balance.list', compact('infoPfr'));
+	}
+
+	public function addNewBalance(Request $request, $idPfr)
+	{
+		return response()->json($request);
 	}
 }
