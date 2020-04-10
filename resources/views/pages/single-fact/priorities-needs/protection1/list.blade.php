a0c60cd56e9b9efda5ac032aa445b9bede0d7b4c
ManhTu <gautrangcb91@gmail.com>
2020-04-10 17:27:35 +0700

    add protection 1

--- resources/views/pages/single-fact/priorities-needs/protection1/list.blade.php
+++ resources/views/pages/single-fact/priorities-needs/protection1/list.blade.php
@@ -6,7 +6,7 @@
         <h4>PROTECTION - Income Protection Upon Death</h4>
     </div>
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <form name="protection1" id='protection1' class="" method="post" action="" data-parsley-validate>
+        <form name="protection1" id='protection1' class="" method="post" action="{{route('single_fact.priorities-needs.priotection_1', $infoPfr->id)}}" data-parsley-validate>
             @csrf
             <table id="blance-table" class="table table-content table-style1" style="width:100%">
                 <thead>
@@ -19,73 +19,73 @@
                 <tbody>
                     <tr>
                         <td>Annual Amount Needed ($)</td>
-                        <td><input type="number" class="form-control" id="annual-amount-client1" name="annual-amount-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="annual-amount-dependent1" name="annual-amount-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="annual_amount_client1" name="annual_amount_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="annual_amount_dependent1" name="annual_amount_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>Number of Years Needed</td>
-                        <td><input type="number" class="form-control" id="years-needed-client1" name="years-needed-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="years-needed-dependent1" name="years-needed-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="years_needed_client1" name="years_needed_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="years_needed_dependent1" name="years_needed_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>Net Rate of Return (adjusted for inflation)</td>
-                        <td><input type="number" class="form-control" id="net-rate-client1" name="net-rate-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="net-rate-dependent1" name="net-rate-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="net_rate_client1" name="net_rate_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="net_rate_dependent1" name="net_rate_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>A. CAPITAL SUM REQUIRED ($)</td>
-                        <td><input type="number" class="form-control" id="capital-sum-client1" name="capital-sum-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="capital-sum-dependent1" name="acapital-sum-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="capital_sum_client1" name="capital_sum_client1" placeholder="$" value="" readonly></td>
+                        <td><input type="number" class="form-control" id="capital_sum_dependent1" name="acapital_sum_dependent1" placeholder="$" value="" readonly></td>
                     </tr>
                     <tr>
                         <td>Final Expenses ($)</td>
-                        <td><input type="number" class="form-control" id="final-expenses-client1" name="final-expenses-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="final-expenses-dependent1" name="final-expenses-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="final_expenses_client1" name="final_expenses_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="final_expenses_dependent1" name="final_expenses_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>Emergency Fund ($)</td>
-                        <td><input type="number" class="form-control" id="emergency-client1" name="emergency-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="emergency-dependent1" name="emergency-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="emergency_client1" name="emergency_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="emergency_dependent1" name="emergency_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>Mortgage ($)</td>
-                        <td><input type="number" class="form-control" id="mortgage-client1" name="mortgage-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="mortgage-dependent1" name="mortgage-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="mortgage_client1" name="mortgage_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="mortgage_dependent1" name="mortgage_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>Personal Debts ($)</td>
-                        <td><input type="number" class="form-control" id="personal-debts-client1" name="personal-debts-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="personal-debts-dependent1" name="personal-debts-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="personal_debts_client1" name="personal_debts_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="personal_debts_dependent1" name="personal_debts_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>Others ($)</td>
-                        <td><input type="number" class="form-control" id="others-client1" name="others-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="others-dependent1" name="others-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="others_client1" name="others_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="others_dependent1" name="others_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>B. TOTAL CASH OUTFLOW ($)</td>
-                        <td><input type="number" class="form-control" id="total-cash-client1" name="total-cash-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="total-cash-dependent1" name="total-cash-dependent1" placeholder="$" value="" readonly></td>
+                        <td><input type="number" class="form-control" id="total_cash_client1" name="total_cash_client1" placeholder="$" value="" readonly></td>
+                        <td><input type="number" class="form-control" id="total-cash_dependent1" name="total_cash_dependent1" placeholder="$" value="" readonly></td>
                     </tr>
                     <tr>
                         <td>TOTAL ( A + B ) ($)</td>
-                        <td><input type="number" class="form-control" id="total-ab-client1" name="total-ab-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="total-ab-dependent1" name="total-ab-dependent1" placeholder="$" value="" readonly></td>
+                        <td><input type="number" class="form-control" id="total_ab_client1" name="total_ab_client1" placeholder="$" value="" readonly></td>
+                        <td><input type="number" class="form-control" id="total_ab_dependent1" name="total_ab_dependent1" placeholder="$" value="" readonly></td>
                     </tr>
                     <tr>
                         <td>Less: Existing Insurance Coverage on Deaths ($)</td>
-                        <td><input type="number" class="form-control" id="insurance-coverage-client1" name="insurance-coverage-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="insurance-coverage-dependent1" name="insurance-coverage-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="insurance_coverage_client1" name="insurance_coverage_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="insurance_coverage_dependent1" name="insurance_coverage_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>Less: Existing Resources ($) (if any)</td>
-                        <td><input type="number" class="form-control" id="existing-resources-client1" name="existing-resources-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="existing-resources-dependent1" name="existing-resources-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="existing_resources_client1" name="existing_resources_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="existing_resources_dependent1" name="existing_resources_dependent1" placeholder="$" value="" ></td>
                     </tr>
                     <tr>
                         <td>Net Amount Required ($)</td>
-                        <td><input type="number" class="form-control" id="net-amount-client1" name="net-amount-client1" placeholder="$" value="" ></td>
-                        <td><input type="number" class="form-control" id="net-amount-dependent1" name="net-amount-dependent1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="net_amount_client1" name="net_amount_client1" placeholder="$" value="" ></td>
+                        <td><input type="number" class="form-control" id="net_amount_dependent1" name="net_amount_dependent1" placeholder="$" value="" ></td>
                     </tr>
                      <tr>
                         <td>Additional Notes</td>
